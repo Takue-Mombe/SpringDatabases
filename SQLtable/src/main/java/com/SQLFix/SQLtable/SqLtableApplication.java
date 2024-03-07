987bb3eed6027c7a7244f813d5b17d61cd981aec
mombe <h220462g@hit.ac.zw>
2024-03-07 14:44:05 +0200

    Initial Project

---
+++ SQLtable/src/main/java/com/SQLFix/SQLtable/SqLtableApplication.java
@@ -0,0 +1,13 @@
+package com.SQLFix.SQLtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqLtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqLtableApplication.class, args);
+	}
+
+}
---
+++ SQLtable/src/test/java/com/SQLFix/SQLtable/SqLtableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SQLFix.SQLtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqLtableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
